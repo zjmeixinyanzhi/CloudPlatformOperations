688403e60103807013b3cfffcc4c71a96afba830
Zhang Jie <zhangjiewanwansui@gmail.com>
2017-04-25 16:41:18 +0800

    Delete test.py

--- bin/test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-from openstack import connection
-conn = connection.Connection(auth_url="http://192.168.100.201:5000/v3",
-                             project_name="admin",
-                             username="admin",
-                             password="123456")
-for container in conn.object_store.containers():
-   print(container.name)
