3ea328beab9d4558a54ccf6c12a528d674e0f0b3
zjmeixinyanzhi <zhangjiewanwansui@gmail.com>
2017-12-05 16:15:25 +0800

    add nova ssh

--- bin/set-nova-ssh.sh
+++ bin/set-nova-ssh.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
-declare -A hypervisor_map=(["compute01"]="172.20.103.4" ["compute02"]="172.20.103.5" ["compute03"]="172.20.103.6" ["compute04"]="172.20.103.7" ["compute05"]="172.20.103.8" ["compute06"]="172.20.103.11" ["compute07"]="172.20.103.12" ["compute08"]="172.20.103.13" ["compute09"]="172.20.103.14" ["compute10"]="172.20.103.15" ["compute11"]="172.20.103.16");
-for host in ${hypervisor_map[@]};
-do
-  ssh $host /bin/bash << EOF
-  su - nova 
-  whoami
+./pssh-exe A "usermod -s /bin/bash nova"
+su - nova -c "ssh-keygen -t rsa"
+cat << EOF > /var/lib/nova/.ssh/config
+Host *
+    StrictHostKeyChecking no
+    UserKnownHostsFile=/dev/null
 EOF
-done 
+cat /var/lib/nova/.ssh/id_rsa.pub > /var/lib/nova/.ssh/authorized_keys
+chmod 600 /var/lib/nova/.ssh/authorized_keys
+./scp-exe H /var/lib/nova/.ssh/ /var/lib/nova/
+./pssh-exe H "chown nova:nova -R /var/lib/nova/.ssh/"
+### Check SSH
+su - nova -c "ssh nova@compute01 cd ~ && pwd"
